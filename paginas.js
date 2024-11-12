7fdc5312697b4de06821046271ff53601576a9c6
ximosa <ximosa@gmail.com>
2024-11-12 18:44:28 +0000

    cambio

--- paginas.js
+++ paginas.js
@@ -28,10 +28,10 @@ async function displayPages() {
 
   if (pages.length > 0) {
     paginasList.innerHTML = pages.map(page => `
-      <div class="responsive large-padding">
-        <h2><a href="pagina.html?slug=${page.slug}" data-link>${page.title}</a></h2>
-      </div>
-      <div class="medium-space"></div>
+      <div class="s12 m6 l6 large-padding center-align">
+        <a class="button responsive border small-round medium-elevate" href="pagina.html?slug=${page.slug}" data-link>${page.title}</a>
+        </div>
+    
     `).join('');
   } else {
     paginasList.innerHTML = '<p>No hay páginas para mostrar.</p>';
